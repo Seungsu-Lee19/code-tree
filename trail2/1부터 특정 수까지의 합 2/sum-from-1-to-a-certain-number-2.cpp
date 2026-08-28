e5303d7e45810c093469157d27dc8a5068e0d053
Seungsu-Lee19 <78408527+Seungsu-Lee19@users.noreply.github.com>
2026-08-28 15:29:13 +0900

    [Passed] 1부터 특정 수까지의 합 2 (1ms, 0MB)

---
+++ trail2/1부터 특정 수까지의 합 2/sum-from-1-to-a-certain-number-2.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int N;
+
+int func(int n, int s){
+    if(n == 0) return s;
+
+    return func(n - 1, s + n);
+}
+
+int main() {
+    cin >> N;
+
+    // Please write your code here.
+    cout << func(N, 0);
+
+    return 0;
+}
