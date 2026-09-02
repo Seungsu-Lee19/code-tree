abfd8bffb3b73d624599affe2473fcaa83a15d4f
Seungsu-Lee19 <78408527+Seungsu-Lee19@users.noreply.github.com>
2026-09-02 15:27:37 +0900

    [Passed] 빙빙 돌며 정수로 사각형 채우기 2 (2ms, 0MB)

---
+++ trail2/빙빙 돌며 정수로 사각형 채우기 2/snail-number-square-2.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+
+using namespace std;
+
+int n, m;
+
+int main() {
+    cin >> n >> m;
+
+    // Please write your code here.
+    int dx[4] = {1, 0, -1, 0};
+    int dy[4] = {0, 1, 0, -1};
+    int dir = 0;
+
+    int x = 0;
+    int y = 0;
+
+    int i = 1;
+    int grid[n][m] = {};
+    while(1){
+        if(i > n * m) break;
+
+        grid[x][y] = i;
+
+        int nx = x + dx[dir];
+        int ny = y + dy[dir];
+
+        if(nx < 0 || ny < 0 || nx >= n || ny >= m){
+            dir = (dir + 1) % 4;
+        }
+        else if(grid[nx][ny] != 0)
+            dir = (dir + 1) % 4;
+
+        x = x + dx[dir];
+        y = y + dy[dir];
+        i++;
+        
+        // int nx = x + dx[dir];
+        // int ny = y + dy[dir];
+    }
+
+    for(int i = 0; i < n; i++){
+        for(int j = 0; j < m; j++){
+            cout << grid[i][j] << " ";
+        }
+        cout << "\n";
+    }
+
+    return 0;
+}
