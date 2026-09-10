d738a43292508aac0ee4cce0c230c80adf34d3e8
Seungsu-Lee19 <78408527+Seungsu-Lee19@users.noreply.github.com>
2026-09-10 15:22:16 +0900

    [Passed] 벽이 있는 충돌 실험 (2ms, 0MB)

--- trail4/벽이 있는 충돌 실험/collision-experiment-with-wall.cpp
+++ trail4/벽이 있는 충돌 실험/collision-experiment-with-wall.cpp
@@ -1,61 +1,52 @@
 #include <iostream>
+#include <vector>
+#include <tuple>
 
 using namespace std;
 
 int T;
 int N, M;
+int x;
+int y;
+char d;
 
 int dx[4] = {0, -1, 0, 1};
 int dy[4] = {-1, 0, 1, 0};
 
-void move(int (&arr)[51][51][4], int (&temp)[51][51][4]){
-    for(int k = 0; k < 4; k++){
-        for(int i = 1; i <= N; i++){
-            for(int j = 1; j <= N; j++){
-                if(arr[i][j][k]){
-                    int nx = i + dx[k];
-                    int ny = j + dy[k];
+vector<tuple<int, int, int>> ball;
 
-                    if(nx < 1 || ny < 1 || nx > N || ny > N) {
-                        temp[i][j][(k + 2) % 4]++;
-                        continue;
-                    }
+tuple<int, int, int> moveBall(int x, int y, int d){
+    int nx = x + dx[d];
+    int ny = y + dy[d];
 
-                    temp[nx][ny][k]++;
-                }
-            }
-        }
-    }
+    if(nx < 1 || ny < 1 || nx > N || ny > N) return {x, y, (d + 2) % 4};
 
-    return;
+    return {nx, ny, d};
 }
 
-int remove_ball(int (&arr)[51][51][4], int (&temp)[51][51][4]){
-    int ret = 0;
-
-    for(int i = 1; i <= N; i++){
-        for(int j = 1; j <= N; j++){
-            int cnt = 0;
-            int p = 0;
-            for(int k = 0; k < 4; k++){
-                if(temp[i][j][k]) {
-                    cnt++;
-                    p = k;
-                }
-                arr[i][j][k] = 0;
-            }
-
-
-            if(cnt == 1) {
-                arr[i][j][p] = 1;
-                ret++;
-            }
-        }
+void moveAll(){
+    for(int i = 0; i < ball.size(); i++){
+        auto [x, y, d] = ball[i];
+        ball[i] = moveBall(x, y, d);
     }
-
-    return ret;
 }
 
+void RemoveDuplicate(){
+    int grid[51][51] = {0, };
+    vector<tuple<int, int, int>> temp;
+    
+    for(int i = 0; i < ball.size(); i++){
+        auto [x, y, d] = ball[i];
+        grid[x][y]++;
+    }
+
+    for(int i = 0; i < ball.size(); i++){
+        auto [x, y, d] = ball[i];
+        if(grid[x][y] < 2) temp.push_back({x, y, d});
+    }
+
+    ball = temp;
+}
 
 int main() {
     cin >> T;
@@ -63,58 +54,22 @@ int main() {
     for (int t = 0; t < T; t++) {
         cin >> N >> M;
 
-        int grid[51][51][4] = {0, };
+        ball.clear();
         for (int i = 0; i < M; i++) {
-            char d;
-            int x;
-            int y;
             cin >> x >> y >> d;
 
-            if(d == 'L') grid[x][y][0] = 1;
-            else if(d == 'U') grid[x][y][1] = 1;
-            else if(d == 'R') grid[x][y][2] = 1;
-            else grid[x][y][3] = 1;
+            if(d == 'L') ball.push_back({x, y, 0});
+            else if(d == 'R') ball.push_back({x, y, 2});
+            else if(d == 'U') ball.push_back({x, y, 1});
+            else ball.push_back({x, y, 3});
         }
 
-        int cnt = 0;
-        int prev = 0;
-        while(1){
-
-            // 구슬 이동
-            int temp[51][51][4] = {0, };
-            move(grid, temp);
-            // for(int i = 1; i <= N; i++) {
-            //     for(int dir = 0; dir < 4; dir++) {
-            //         for(int j = 1; j <= N; j++) {
-            //             cout << temp[i][j][dir] << " ";
-            //         }
-
-            //         if(dir != 3)
-            //             cout << "- ";
-            //     }
-            //     cout << '\n';
-            // }
-            // cout << "---------------------------------------\n";
-
-            // 구슬 제거
-            int ch = remove_ball(grid, temp);
-
-            // 구슬 이동해도 의미 있나? check
-            if(ch == prev) cnt++;
-            else{
-                cnt = 0;
-                prev = ch;
-            }
-
-            if(cnt > 2 * N) {
-                cout << ch << endl;
-                break;
-            }
-            // if(check(temp)) break;
+        for(int k = 0; k < 2 * N; k++){
+            moveAll();
+            RemoveDuplicate();
         }
 
-        // cout << cnt << endl;        
-
+        cout << ball.size() << endl;
     }
 
     return 0;
