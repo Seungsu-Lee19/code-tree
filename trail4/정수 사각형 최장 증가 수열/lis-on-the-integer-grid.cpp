875c07eb00ebccb53f967f5c9aca22c6b1e33d0b
Seungsu-Lee19 <78408527+Seungsu-Lee19@users.noreply.github.com>
2026-09-22 15:23:47 +0900

    [Passed] 정수 사각형 최장 증가 수열 (100ms, 2MB)

---
+++ trail4/정수 사각형 최장 증가 수열/lis-on-the-integer-grid.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int n;
+int grid[500][500];
+int dp[500][500];
+
+int dx[4] = {0, 0, -1, 1};
+int dy[4] = {-1, 1, 0, 0};
+
+int dfs(int x, int y){
+    if(dp[x][y] != 0) return dp[x][y];
+
+    dp[x][y] = 1;
+
+    for(int i = 0; i < 4; i++){
+        int nx = x + dx[i];
+        int ny = y + dy[i];
+
+        if(nx < 0 || ny < 0 || nx >= n || ny >= n) continue;
+
+        if(grid[x][y] < grid[nx][ny]){
+            dp[x][y] = max(dp[x][y], dfs(nx, ny) + 1);
+        }
+    }
+
+    return dp[x][y];
+}
+
+int main() {
+    cin >> n;
+
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            cin >> grid[i][j];
+        }
+    }
+
+    // Please write your code here.
+
+    int ans = 0;
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            ans = max(ans, dfs(i, j));
+        }
+    }
+
+    cout << ans;
+
+    return 0;
+}
