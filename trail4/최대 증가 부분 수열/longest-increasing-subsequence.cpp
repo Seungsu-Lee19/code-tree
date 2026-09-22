5fc8b5e4cbf78543406b8314f72a85c3f5cd4e0f
Seungsu-Lee19 <78408527+Seungsu-Lee19@users.noreply.github.com>
2026-09-22 17:32:00 +0900

    [Passed] 최대 증가 부분 수열 (1ms, 0MB)

---
+++ trail4/최대 증가 부분 수열/longest-increasing-subsequence.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int N;
+int M[1000];
+
+int main() {
+    cin >> N;
+    for (int i = 0; i < N; i++) {
+        cin >> M[i];
+    }
+
+    // Please write your code here.
+    
+    vector<int> dp(N, 1);
+    for(int i = 0; i < N; i++){
+        for(int j = 0; j <= i - 1; j++){
+            if(M[j] < M[i]){
+                dp[i] = max(dp[i], dp[j] + 1);
+            }
+        }
+    }
+
+    cout << *max_element(dp.begin(), dp.end());
+
+    return 0;
+}
