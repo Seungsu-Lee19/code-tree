7954dc6402795549c442340ae1d66eee61900737
Seungsu-Lee19 <78408527+Seungsu-Lee19@users.noreply.github.com>
2026-09-23 10:32:16 +0900

    [Passed] 겹치지 않게 선분 고르기 2 (2ms, 0MB)

--- trail4/겹치지 않게 선분 고르기 2/select-segments-without-overlap-2.cpp
+++ trail4/겹치지 않게 선분 고르기 2/select-segments-without-overlap-2.cpp
@@ -1,7 +1,6 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
-#include <utility>
 
 using namespace std;
 
@@ -10,26 +9,32 @@ int n;
 int main() {
     cin >> n;
 
-    vector<pair<int, int>> line;
-    int x1, x2;
-    for (int i = 0; i < n; i++) {
+    vector<pair<int, int>> v;
+
+    for(int i = 0; i < n; i++) {
+        int x1, x2;
         cin >> x1 >> x2;
-        line.push_back({x1, x2});
+
+        // {끝점, 시작점}
+        v.push_back({x2, x1});
     }
 
-    // Please write your code here.
-    sort(line.begin(), line.end());
-    vector<int> dp(n, 1);
+    // 끝점 기준 오름차순 정렬
+    sort(v.begin(), v.end());
 
-    for(int i = 0; i < n; i++){
-        for(int j = 0; j < i; j++){
-            // if(i == j) continue;
+    int cnt = 0;
+    int lastEnd = -1;
 
-            if(line[i].first > line[j].second) dp[i] = max(dp[i], dp[j] + 1);
+    for(auto [end, start] : v) {
+
+        // 이전 구간과 겹치지 않는다면 선택
+        if(start > lastEnd) {
+            cnt++;
+            lastEnd = end;
         }
     }
 
-    cout << *max_element(dp.begin(), dp.end());
+    cout << cnt;
 
     return 0;
 }
